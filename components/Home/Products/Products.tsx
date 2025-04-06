73f4933648269fa615541c3aa4fa6cd064b9b8af
sinaa.afshar@gmail.com <sinaa.afshar@gmail.com>
2025-04-06 15:29:39 +0330

    get product properties from django json

--- components/Home/Products/Products.tsx
+++ components/Home/Products/Products.tsx
@@ -1,17 +1,50 @@
+'use client';
+
+
 import { ProductsItem } from '@/constant/Productsitem'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductsCard from './ProductsCard';
 import Link from 'next/link';
 import { Item } from '@radix-ui/themes/components/checkbox-group.primitive';
 
+export type Props ={
+  id: string;
+  image_url: string;
+  title: string;
+  description: string;
+  slug:string;
+  price:string;
+  offer_price:string;
+};
 
 const Products = () => {
+  const [products, setProducts] = useState<Props[]>([]);
+  const getProperties = async () => {
+    const url = 'http://localhost:8000/api/products/'
+
+    await fetch(url, {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then((json) => {
+        console.log('json',json)
+
+        setProducts(json.data)
+      })
+      .catch((error) => {
+        console.log('error', error);
+      })
+  };
+
+  useEffect (() => {
+    getProperties();
+  }, []);
   return (
     <div className='max-w-[1500px] mx-auto px-6 bg-white dark:bg-gray-950 pt-16 pb-16'>
         <div className='w-[80%] mx-auto mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10'>
-          { ProductsItem.map((item) => {
+          { products.map((product) => {
             return (
-              <ProductsCard id={item.id} key={item.id} url={item.url} label={item.label} iconurl={item.iconurl} underline={item.underline} />
+              <ProductsCard id={product.id} key={product.id} slug={product.slug} title={product.title} imageurl={product.image_url} description={product.description} price={product.price} offerprice={product.offer_price} />
             )
           })}
         </div>
--- components/Home/Products/ProductsCard.tsx
+++ components/Home/Products/ProductsCard.tsx
@@ -3,33 +3,36 @@ import Link from 'next/link';
 import React from 'react'
 
 type Props ={
-    id: number;
-    url: string;
-    label: string;
-    iconurl:string;
-    underline:string;
+    id: string;
+    imageurl: string;
+    title: string;
+    description: string;
+    slug: string;
+    price: string;
+    offerprice: string;
   };
 
-const ProductsCard = ({id, url, label, iconurl,underline}: Props) => {
+const ProductsCard = ({id, imageurl, title,description,slug, price,offerprice}: Props) => {
   return (
     <div key={id}>
-        <Link  href={url} className="cursor-pointer">
+        <Link  href='/products/${slug}' className="cursor-pointer">
             <div className='relative mx-auto overflow-hidden aspect-square rounded-xl'>
                   <Image 
                     fill
                     sizes='(max-width:768px) 768px,(max-width:1200px) 768px, 768px'
-                    src={iconurl} 
-                    alt={label} 
+                    src={imageurl} 
+                    alt={title} 
                     className='hover:scale-110 object-cover transition h-full w-full'
                   />
             </div>
             <div className='mt-2'>
-              <p className='font-bold text-lg text-center'>{label}</p>
+              <p className='font-bold text-lg text-center'>{title}</p>
             </div>
             <div className='mt-2'>
-              <p className='font-bold text-gray-700 dark:text-gray-300 text-sm text-center'><strong>300 هزار تومان</strong></p>
+              <p className='font-bold text-gray-700 dark:text-gray-300 text-sm text-center'><strong>{price} هزار تومان</strong></p>
+              <p className='font-bold text-gray-700 dark:text-gray-300 text-sm text-center'><strong>{offerprice} هزار تومان</strong></p>
             </div>
-            <div><span className='text-sm'>{underline}</span></div>
+            <div><span className='text-sm'>{description}</span></div>
         </Link>
     </div>
   )
